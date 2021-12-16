87a4efd18e3ea6928aa4e9d3c5cd786ff7d4d996
EstrelaE <estrela.palma@hotmail.com>
2021-12-16 17:10:21 +0000

    Updated JS forecast function using the weather API data

--- src/app.js
+++ src/app.js
@@ -21,33 +21,60 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast(params) {
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
                <div class="col-2">
-                 <div class="weather-forecast-date">${day}</div>
+                 <div class="weather-forecast-date">${formatDay(
+                   forecastDay.dt
+                 )}</div>
                    <img
-                     src="https://openweathermap.org/img/wn/10n@2x.png"
+                     src="https://openweathermap.org/img/wn/${
+                       forecastDay.weather[0].icon
+                     }@2x.png"
                      alt=""
                      width="42px"
                    />
                  <div class="weather-forecast-temperature">
-                   <span class="weather-forecast-temperature-max">18º </span>
-                   <span class="weather-forecast-temperature-min">12º </span>
+                   <span class="weather-forecast-temperature-max">${Math.round(
+                     forecastDay.temp.max
+                   )}º</span>
+                   <span class="weather-forecast-temperature-min">${Math.round(
+                     forecastDay.temp.min
+                   )}º </span>
                  </div>
                </div>                
                    `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
 
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = `a27b2f052c3b6156c214f3a13d66d3bf`;
+  let apiEndPoint = `https://api.openweathermap.org/data/2.5/onecall`;
+  let apiUrl = `${apiEndPoint}?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -77,6 +104,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -126,4 +155,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("Angola");
-displayForecast();
